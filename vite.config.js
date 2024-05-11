651525c7cc816aff2576843ba6e83908ddcd0521
Kiyan Alav <kiyanalavi1379@gmail.com>
2024-05-11 20:33:55 +0330

    V0.0.0.1:
    - initial structure add
    - sidebar desktop finish

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pages: {
+    "/": {
+      entry: "index.html",
+    },
+    "/about": {
+      entry: "src/pages/products.html",
+    },
+    "/contact": {
+      entry: "src/pages/users.html",
+    },
+  },
+};
